79e25c987ccb4eb0a69502522411a1641568ce23
Delta <ho.pe.ful.ar.tist.111@gmail.com>
2026-01-18 20:47:47 +0300

    0.00..4

---
+++ Modular Systems/Scripts/hitbox_component.gd
@@ -0,0 +1,4 @@
+extends Area2D
+class_name HurtboxComponent
+
+@export var damage_amount: float
--- Modular Systems/Scripts/hurtbox_component.gd
+++ Modular Systems/Scripts/hurtbox_component.gd
@@ -1,4 +1,12 @@
 extends Area2D
-class_name HurtboxComponent
+class_name HitboxComponent
 
-@export var damage_amount: float
+@export var health_component : HealthComponent
+
+
+func _on_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	health_component.damage(area.damage_amount)
+
+
+func _on_area_shape_exited(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	pass # Replace with function body.
