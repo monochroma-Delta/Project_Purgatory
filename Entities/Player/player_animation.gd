bbe6299fd420aeaf131f73d024ce53f269ea9592
Delta <ho.pe.ful.ar.tist.111@gmail.com>
2026-02-10 21:56:45 +0300

    asd

--- Entities/Player/player_animation.gd
+++ Entities/Player/player_animation.gd
@@ -1,15 +1,1 @@
 extends Node
-
-
-
-@onready var silver_spear: Sprite2D = $RightSprites/Body/Arm/Gauntlet/SilverSpear
-@onready var silver_sword: Sprite2D = $RightSprites/Body/Arm/Gauntlet/SilverSword
-@onready var silver_great_sword: Sprite2D = $RightSprites/Body/Arm/Gauntlet/SilverGreatSword
-@onready var gauntlet: Sprite2D = $RightSprites/Body/Arm/Gauntlet
-@onready var gauntlet_1: Sprite2D = $RightSprites/Body/Arm1/Gauntlet1
-
-
-func weapon_reparent(path: NodePath) -> void:
-	
-	silver_spear.reparent(get_node(path))
-	
