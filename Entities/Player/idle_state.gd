eceb5bfbdbabe7a782306780dafda4c7be51e3d0
Delta <ho.pe.ful.ar.tist.111@gmail.com>
2026-02-11 22:37:11 +0300

    .

--- Entities/Player/idle_state.gd
+++ Entities/Player/idle_state.gd
@@ -1,5 +1,11 @@
 extends State
 
+func enter(previous_state_path: String, data := {}) -> void:
+	owner.body_animation_tree["parameters/BodyAnimations/3/conditions/idle"] = true
+
+func exit() -> void:
+	owner.body_animation_tree["parameters/BodyAnimations/3/conditions/idle"] = false
+
 func physics_update(_delta: float) -> void:
 	owner.velocity = owner.velocity.lerp(Vector2.ZERO, owner.accel) 
 	if Input.get_vector("LEFT","RIGHT","UP","DOWN") != Vector2.ZERO:
--- Entities/Player/move_state.gd
+++ Entities/Player/move_state.gd
@@ -1,5 +1,11 @@
 extends State
 
+func enter(previous_state_path: String, data := {}) -> void:
+	owner.body_animation_tree["parameters/BodyAnimations/3/conditions/sprint"] = true
+
+func exit() -> void:
+	owner.body_animation_tree["parameters/BodyAnimations/3/conditions/sprint"] = false
+
 func physics_update(_delta: float) -> void:
 	owner.velocity = owner.velocity.lerp(owner.direction.normalized() * owner.speed , owner.accel)
 	
