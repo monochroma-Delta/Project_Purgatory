c18ef3d04990b0747b5706b4e2f7646d64a7b396
Delta <ho.pe.ful.ar.tist.111@gmail.com>
2026-02-22 15:41:23 +0300

    Reapply "iri kılıc eklendi!!1!"
    This reverts commit 11914fc9d5b87d9ba04f505912349d66796d15d1.

--- Entities/Player/idle_state.gd
+++ Entities/Player/idle_state.gd
@@ -7,12 +7,22 @@ func physics_update(_delta: float) -> void:
 	if owner.spear == true:
 		owner.body_animation_tree["parameters/BodySpearAnimations/0/Sprint/blend_amount"] = lerpf(owner.body_animation_tree["parameters/BodySpearAnimations/0/Sprint/blend_amount"],0, 0.15)   
 		owner.body_animation_tree["parameters/BodySpearAnimations/1/Sprint/blend_amount"] = lerpf(owner.body_animation_tree["parameters/BodySpearAnimations/1/Sprint/blend_amount"],0, 0.15)   
-		owner.body_animation_tree["parameters/BodySpearAnimations/2/Sprint/blend_amount"] = lerpf(owner.body_animation_tree["parameters/BodySpearAnimations/2/Sprint/blend_amount"],0, 1)   
+		owner.body_animation_tree["parameters/BodySpearAnimations/2/Sprint/blend_amount"] = lerpf(owner.body_animation_tree["parameters/BodySpearAnimations/2/Sprint/blend_amount"],0, 0.15)   
 		owner.body_animation_tree["parameters/BodySpearAnimations/3/Sprint/blend_amount"] = lerpf(owner.body_animation_tree["parameters/BodySpearAnimations/3/Sprint/blend_amount"],0, 0.15)
+	elif owner.longsword == true:
+		owner.body_animation_tree["parameters/BodyLongswordAnimations/0/Sprint/blend_amount"] = lerpf(owner.body_animation_tree["parameters/BodyLongswordAnimations/0/Sprint/blend_amount"],0, 0.15)   
+		owner.body_animation_tree["parameters/BodyLongswordAnimations/1/Sprint/blend_amount"] = lerpf(owner.body_animation_tree["parameters/BodyLongswordAnimations/1/Sprint/blend_amount"],0, 0.15)   
+		owner.body_animation_tree["parameters/BodyLongswordAnimations/2/Sprint/blend_amount"] = lerpf(owner.body_animation_tree["parameters/BodyLongswordAnimations/2/Sprint/blend_amount"],0, 0.15)   
+		owner.body_animation_tree["parameters/BodyLongswordAnimations/3/Sprint/blend_amount"] = lerpf(owner.body_animation_tree["parameters/BodyLongswordAnimations/3/Sprint/blend_amount"],0, 0.15)
+	elif owner.greatsword == true:
+		owner.body_animation_tree["parameters/BodyGreatswordAnimations/0/Sprint/blend_amount"] = lerpf(owner.body_animation_tree["parameters/BodyGreatswordAnimations/0/Sprint/blend_amount"],0, 0.15)   
+		owner.body_animation_tree["parameters/BodyGreatswordAnimations/1/Sprint/blend_amount"] = lerpf(owner.body_animation_tree["parameters/BodyGreatswordAnimations/1/Sprint/blend_amount"],0, 0.15)   
+		owner.body_animation_tree["parameters/BodyGreatswordAnimations/2/Sprint/blend_amount"] = lerpf(owner.body_animation_tree["parameters/BodyGreatswordAnimations/2/Sprint/blend_amount"],0, 0.15)   
+		owner.body_animation_tree["parameters/BodyGreatswordAnimations/3/Sprint/blend_amount"] = lerpf(owner.body_animation_tree["parameters/BodyGreatswordAnimations/3/Sprint/blend_amount"],0, 0.15)
 	
 	owner.head_animation_tree["parameters/HeadAnimations/0/Sprint/blend_amount"] = lerpf(owner.head_animation_tree["parameters/HeadAnimations/0/Sprint/blend_amount"],0, 0.15)  
 	owner.head_animation_tree["parameters/HeadAnimations/1/Sprint/blend_amount"] = lerpf(owner.head_animation_tree["parameters/HeadAnimations/1/Sprint/blend_amount"],0, 0.15)  
-	#owner.head_animation_tree["parameters/HeadAnimations/2/Sprint/blend_amount"] = lerpf(owner.head_animation_tree["parameters/HeadAnimations/2/Sprint/blend_amount"],0, 0.15)  
+	owner.head_animation_tree["parameters/HeadAnimations/2/Sprint/blend_amount"] = lerpf(owner.head_animation_tree["parameters/HeadAnimations/2/Sprint/blend_amount"],0, 0.15)  
 	owner.head_animation_tree["parameters/HeadAnimations/3/Sprint/blend_amount"] = lerpf(owner.head_animation_tree["parameters/HeadAnimations/3/Sprint/blend_amount"],0, 0.15)  
 	
 	
